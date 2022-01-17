11ab3658e10eabe32be20c0419430cde792ac27c
TommasoStroppa <info.tommasostroppa@gmail.com>
2022-01-17 14:54:03 +0100

    Inizio Extract Meteo

--- HeyTravel/API/Functions/ScrapingRepository.cs
+++ HeyTravel/API/Functions/ScrapingRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Models;
+using HtmlAgilityPack;
 
 namespace API.Functions
 {
@@ -10,6 +11,30 @@ namespace API.Functions
     {
         public IEnumerable<Meteo> ExtractMeteo (string stato, string città)
         {
+            string link=default(string);
+            bool solostato = true;
+            if (città=="")
+            {
+                link = $"https://www.climieviaggi.it/clima/{stato.ToLower()}";
+            }
+            else
+            {
+                string cittàLower = città.ToLower();
+                if (cittàLower.Contains(" "))
+                {
+                    cittàLower.Replace(' ', '-');
+                }                    
+                link = $"https://www.climieviaggi.it/clima/{stato.ToLower()}/{cittàLower}";
+                solostato = false;
+            }
+
+            HtmlNodeCollection NodesCities;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load(link);
+
+            NodesCities = document.DocumentNode.SelectNodes("/html/body/form/div[7]/div[1]/div[4]/div[1]/table[contains(@class, 'cities')]");
+
+
             return null;
         }
     }

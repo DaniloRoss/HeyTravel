109d52bc244fe684e2efe32c2c583aae97cb04e5
DaniloRoss <62284476+DaniloRoss@users.noreply.github.com>
2022-01-25 10:59:16 +0100

    Correzione errori

--- HeyTravel/API/Functions/ScrapingRepository.cs
+++ HeyTravel/API/Functions/ScrapingRepository.cs
@@ -13,12 +13,45 @@ namespace API.Functions
     public class ScrapingRepository : IScrapingRepository
     {
         /// <summary>
-        /// Funzione che consulta l'API di Google Translate
+        /// Metodo che dato in input uno stato, restituisce il suo codice ISO a due caratteri
         /// </summary>
-        /// <param name="text"></param>
-        /// <param name="target"></param>
-        /// <param name="source"></param>
-        /// <returns>Testo tradotto</returns>
+        /// <param name="stato">Stato in Inglese di cui estrarre il codice a due caratteri</param>
+        /// <returns></returns>
+        public string ExtractCountryCode(string stato)
+        {
+            string statoInput;
+            string link = "https://www.nationsonline.org/oneworld/country_code_list.htm";
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web?.Load(link);
+
+            stato = stato.ToLower();
+            statoInput = stato;
+
+            if (stato == "russia")
+            {
+                statoInput = "russian federation";
+            }
+            foreach (var tabella in document.DocumentNode.SelectNodes(".//table"))
+            {
+                string idTab = tabella.GetAttributeValue("id", null);
+                string[] split = idTab.Split('-');
+                if (string.Compare(split[2].ToLower(), statoInput.Substring(0, 1)) > 0 || string.Compare(split[2].ToLower(), statoInput.Substring(0, 1)) == 0
+                    || string.Compare(split[3].ToLower(), statoInput.Substring(0, 1)) < 0 || string.Compare(split[3].ToLower(), statoInput.Substring(0, 1)) == 0)
+                {
+                    foreach (var riga in tabella.SelectNodes(".//tr"))
+                    {
+                        if (riga.SelectSingleNode(".//td[contains(@class, 'abs')]").InnerText.Trim().ToLower() == statoInput)
+                        {
+                            return riga.SelectSingleNode(".//td[3]").InnerText.Trim();
+
+                        }
+                    }
+                }
+            }
+            return null;
+
+        }
         public async Task<string> Translate(string text, string target, string source)
         {
             var client = new HttpClient();
@@ -44,13 +77,53 @@ namespace API.Functions
                 var body = response.Content.ReadAsStringAsync();
                 var result = JsonConvert.DeserializeObject<RootTranslation>(body.Result).data.translations[0].translatedText;
                 return result;
-            }           
+            }
         }
-
-        public IEnumerable<Meteo> ExtractMeteo (string stato, string citta)
+        /// <summary>
+        /// Metodo che dato in input uno stato, estrae le prime 20 città per popolazione
+        /// </summary>
+        /// <param name="stato">Stato in Italiano da cui estrarre le città</param>
+        /// <returns></returns>
+        public async Task<IEnumerable<Citta>> ExtractBestCitiesPerCountry(string stato)
         {
-            string link=default(string);
-            if (citta== "&")
+            string statotradotto, codicestato;
+
+            stato = char.ToUpper(stato[0]) + stato.Substring(1).ToLower();
+
+            if (stato.Contains("avorio"))
+            {
+                statotradotto = await Translate(stato, "en", "fr");
+            }
+            else
+            {
+                statotradotto = await Translate(stato, "en", "it");
+            }
+
+            codicestato = ExtractCountryCode(statotradotto);
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&countryIds={codicestato}&sort=-population&languageCode=IT&types=CITY"),
+                Headers =
+                {
+                    { "x-rapidapi-host", "wft-geo-db.p.rapidapi.com" },
+                    { "x-rapidapi-key", "3d0684d35amsh068100b881d194cp1cd704jsn90bc3c996d91" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<RootCitta>(body).data;
+                return result;
+            }
+        }
+        public IEnumerable<Meteo> ExtractMeteo(string stato, string citta)
+        {
+            string link = default(string);
+            if (citta == "&")
             {
                 link = $"https://www.climieviaggi.it/clima/{stato.ToLower()}";
             }
@@ -60,7 +133,7 @@ namespace API.Functions
                 if (cittaLower.Contains(" "))
                 {
                     cittaLower.Replace(' ', '-');
-                }                    
+                }
                 link = $"https://www.climieviaggi.it/clima/{stato.ToLower()}/{cittaLower}";
             }
 
@@ -71,43 +144,43 @@ namespace API.Functions
 
             List<Meteo> eleMeteo = new List<Meteo>();
 
-            if (document==null)
+            if (document == null)
             {
                 return eleMeteo;
             }
 
             NodesTabelle = document.DocumentNode.SelectNodes(".//table");
 
-            if(NodesTabelle==null || document.DocumentNode.SelectSingleNode("//span").InnerText.StartsWith("Errore"))
+            if (NodesTabelle == null || document.DocumentNode.SelectSingleNode("//span").InnerText.StartsWith("Errore"))
             {
                 return eleMeteo;
             }
-            
+
             List<string> eleNomi = new List<string>();
-            foreach(var tabellaNomi in NodesTabelle)
+            foreach (var tabellaNomi in NodesTabelle)
             {
                 string captionNome = tabellaNomi.SelectSingleNode(".//caption").InnerText.Trim();
                 string MeteoCittaNomi = captionNome.Substring(0, captionNome.IndexOf('-') - 1);
-                if(eleNomi.Where(p=>p==MeteoCittaNomi).Count()==0)
+                if (eleNomi.Where(p => p == MeteoCittaNomi).Count() == 0)
                 {
                     eleNomi.Add(MeteoCittaNomi);
                 }
             }
 
-            foreach(string nomecitta in eleNomi) 
+            foreach (string nomecitta in eleNomi)
             {
                 Meteo met = new Meteo();
-                met.Stato = char.ToUpper(stato[0])+stato.Substring(1);
+                met.Stato = char.ToUpper(stato[0]) + stato.Substring(1);
                 met.Citta = nomecitta;
                 foreach (var tabella in NodesTabelle)
                 {
                     string captionNome = tabella.SelectSingleNode(".//caption").InnerText.Trim();
                     string MeteoCittaNomi = captionNome.Substring(0, captionNome.IndexOf('-') - 1);
-                    if(nomecitta==MeteoCittaNomi)
+                    if (nomecitta == MeteoCittaNomi)
                     {
-                        if(tabella.GetClasses().ToList()[0]=="cities")
+                        if (tabella.GetClasses().ToList()[0] == "cities")
                         {
-                            List<Temperature> eleTemperature = new List<Temperature>();                            
+                            List<Temperature> eleTemperature = new List<Temperature>();
                             foreach (HtmlNode riga in tabella.SelectNodes(".//tr[contains(@class, 'min-table')]"))
                             {
                                 Temperature Temperature = new Temperature();
@@ -115,11 +188,11 @@ namespace API.Functions
                                 Temperature.Min = decimal.Parse(riga.ChildNodes[1].InnerText.Trim().ToString());
                                 Temperature.Max = decimal.Parse(riga.ChildNodes[2].InnerText.Trim().ToString());
                                 Temperature.Media = decimal.Parse(riga.ChildNodes[3].InnerText.Trim().ToString());
-                                eleTemperature.Add(Temperature);                                
+                                eleTemperature.Add(Temperature);
                             }
                             met.Temperature = eleTemperature;
                         }
-                        if(tabella.GetClasses().ToList()[0] == "precipit")
+                        if (tabella.GetClasses().ToList()[0] == "precipit")
                         {
                             List<Precipitazioni> elePrecipitazioni = new List<Precipitazioni>();
                             foreach (HtmlNode riga in tabella.SelectNodes(".//tr[contains(@class, 'precipit-table')]"))
@@ -130,16 +203,16 @@ namespace API.Functions
                                 Precipitazioni.Giorni = int.Parse(riga.ChildNodes[2].InnerText.Trim().ToString());
                                 elePrecipitazioni.Add(Precipitazioni);
                             }
-                            met.Precipitazioni = elePrecipitazioni;                            
+                            met.Precipitazioni = elePrecipitazioni;
                         }
-                        if(tabella.GetClasses().ToList()[0] == "sole")
+                        if (tabella.GetClasses().ToList()[0] == "sole")
                         {
-                            List<OreSole> eleOreSole = new List<OreSole>();          
-                            for(int i=2;i<40;i=i+3)
+                            List<OreSole> eleOreSole = new List<OreSole>();
+                            for (int i = 2; i < 40; i = i + 3)
                             {
                                 HtmlNode cella1 = tabella.ChildNodes[i];
-                                HtmlNode cella2 = tabella.ChildNodes[i+1];
-                                HtmlNode cella3 = tabella.ChildNodes[i+2];
+                                HtmlNode cella2 = tabella.ChildNodes[i + 1];
+                                HtmlNode cella3 = tabella.ChildNodes[i + 2];
                                 OreSole OreSole = new OreSole();
                                 OreSole.Mese = cella1.ChildNodes[0].InnerText.Trim().ToString();
                                 OreSole.MediaGiornaliera = decimal.Parse(cella2.InnerText.Trim().ToString());
@@ -148,7 +221,7 @@ namespace API.Functions
                             }
                             met.OreSole = eleOreSole;
                         }
-                        if(tabella.GetClasses().ToList()[0] == "mare")
+                        if (tabella.GetClasses().ToList()[0] == "mare")
                         {
                             List<Mare> eleMare = new List<Mare>();
                             for (int i = 2; i < 27; i = i + 2)
@@ -165,7 +238,7 @@ namespace API.Functions
                     }
                 }
                 eleMeteo.Add(met);
-            }            
+            }
             return eleMeteo;
         }
 

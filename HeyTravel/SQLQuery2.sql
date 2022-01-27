cd19bb440eff95fb1851e06b12d1f2dbc67f26b8
DaniloRoss <danilo.rossi03@gmail.com>
2022-01-27 13:28:19 +0100

    Aggiunta metodo traduzione

---
+++ HeyTravel/SQLQuery2.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE GetIng
+	@Italiano nvarchar(50)
+AS
+	SELECT Inglese
+	FROM Country
+	WHERE Italiano = @Italiano
---
+++ HeyTravel/SQLQuery3.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE GetIta
+	@Inglese nvarchar(50)
+AS
+	SELECT Italiano
+	FROM Country
+	WHERE Inglese = @Inglese

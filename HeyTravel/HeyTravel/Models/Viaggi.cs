f8428d948a244b21151eb33a6f0f62ec1ea2ff6c
TommasoStroppa <info.tommasostroppa@gmail.com>
2022-02-12 10:48:53 +0100

    Correzione

--- HeyTravel/HeyTravel/Models/Viaggi.cs
+++ HeyTravel/HeyTravel/Models/Viaggi.cs
@@ -12,11 +12,9 @@ namespace HeyTravel.Models
         public int ID { get; set; }
         public string StatoPartenza { get; set; }
         public string StatoArrivo { get; set; }
+        public string MeseArrivo { get; set; }
         public Vaccini Vaccinati { get; set; }
         public Casi CasiCovid { get; set; }
-        public Temperature Temp { get; set; }
-        public Precipitazioni Preci { get; set; }
-        public OreSole Sole { get; set; }
-        public Mare Tmare { get; set; }
+        public Meteo Meteo { get; set; }
     }
 }
